b7c67df2bab392722caa2ad04bbd4835e443a882
Kleiton Rufino <kleitonhd@hotmail.com>
2015-06-09 15:18:04 -0300

    - Commit novo pom - Add MessageDAO no modulo personalTrainer

---
+++ personalTrainer/src/main/java/conexao/ConnectionFactory.java
@@ -0,0 +1,10 @@
+package conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://localhost/fj21");
+	}
+}
